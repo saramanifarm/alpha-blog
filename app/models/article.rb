29549633cc54d0a31425c60a1c3c73a05fd0a8e7
saramanifarm <saraman@i.farm>
2021-08-10 12:36:05 +0530

    Article design done

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum:2, maximum: 100}
-    validates :description, presence: true, length: {minimum: 2, maximum: 500}
+    validates :description, presence: true, length: {minimum: 2, maximum: 50000}
 end
